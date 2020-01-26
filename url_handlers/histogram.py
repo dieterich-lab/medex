2692dbd70f475da547c6301bac22e9edf05104cf
Yonasnegash <yonas.negash91@gmail.com>
2020-01-26 20:15:06 +0300

    make error handling for histogram

--- url_handlers/histogram.py
+++ url_handlers/histogram.py
@@ -30,15 +30,19 @@ def post_statistics():
     entity = request.form.get('entity')
     group_by = request.form.get('group_by')
     number_of_bins = request.form.get('number_of_bins')
+    error = None
     if not entity or not group_by or entity == "Choose entity" or group_by == "Choose entity":
         error = "Please select entity and group_by"
 
+    # get joined numerical and categorical values
+    if not error:
+        numeric_df, error = rwh.get_joined_numeric_values([entity], rdb)
+        categorical_df, error = rwh.get_joined_categorical_values([group_by], rdb) if not error else (None, error)
+    if error:
         return render_template('histogram.html', categorical_entities=all_categorical_entities,
-                               numeric_entities=all_numeric_entities,
+                               numeric_entities=all_numeric_entities, selected_entity=entity, group_by=group_by,
                                error=error, )
 
-    numeric_df = rwh.get_joined_numeric_values([entity], rdb)
-    categorical_df = rwh.get_joined_categorical_values([group_by], rdb)
     merged_df = pd.merge(numeric_df, categorical_df, how='inner', on='patient_id')
     min_val = numeric_df[entity].min()
     max_val = numeric_df[entity].max()
