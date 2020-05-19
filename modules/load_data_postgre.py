15286663a83a3da8955268c45dee15877576af27
Magdalena5 <magdalenasmieszek5@gmail.com>
2020-05-19 08:59:17 +0200

    add crosstab instead pivoting

--- modules/load_data_postgre.py
+++ modules/load_data_postgre.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import time
 
 
 def get_categorical_entities(rdb):
@@ -10,19 +11,38 @@ def get_categorical_entities(rdb):
 
 
 def get_values(entity, r):
+#    t0 = time.time()
+#    entity_fin = "'" + "','".join(entity) + "'"
+    entity_fin2 = "''" + "'',''".join(entity) + "''"
+    entity_fin3 = '"' + '" double precision,"'.join(entity) + '" double precision'
+#    sql = """SELECT  "Patient_ID","Key","Value" FROM examination_numerical WHERE "Key" IN ({}) """.format(entity_fin)
+    sql2 = """SELECT  * FROM crosstab ( 'SELECT  "Patient_ID","Key","Value" FROM examination_numerical WHERE "Key" IN ({0})')
+            AS final_result("Patient_ID" TEXT, {1})""".format(entity_fin2,entity_fin3)
+    df = pd.read_sql(sql2, r)
+#    t1 = time.time()
+#    df = df.pivot_table(index="Patient_ID", columns="Key", values="Value", aggfunc=np.mean).reset_index()
+#    df.to_csv('/home/magda/Documents/gen/2.csv', index=False)
+#    t2 = time.time()
+#    total = t1-t0
+#    total2 = t2 - t0
+#    print(total,total2)
+#    print(df)
 
-    entity_fin = "'" + "','".join(entity) + "'"
-    sql = """SELECT  "Patient_ID","Key","Value" FROM examination_numerical WHERE "Key" IN ({}) """.format(entity_fin)
-    df = pd.read_sql(sql, r)
-    df = df.pivot_table(index="Patient_ID", columns="Key", values="Value", aggfunc=np.mean).reset_index()
     return df
 
 
 def get_cat_values(entity,r):
-    entity_fin = "'" + "','".join(entity) + "'"
-    sql = """SELECT  "Patient_ID","Key","Value" FROM examination_categorical WHERE "Key" IN ({}) """.format(entity_fin)
-    df = pd.read_sql(sql, r)
-    df = df.pivot_table(index="Patient_ID", columns="Key", values="Value", aggfunc=min).reset_index()
+#    entity_fin = "'" + "','".join(entity) + "'"
+#    sql = """SELECT  "Patient_ID","Key","Value" FROM examination_categorical WHERE "Key" IN ({}) """.format(entity_fin)
+#    df = pd.read_sql(sql, r)
+#    df = df.pivot_table(index="Patient_ID", columns="Key", values="Value", aggfunc=min).reset_index()
+
+    entity_fin2 = "''" + "'',''".join(entity) + "''"
+    entity_fin3 = '"' + '" double precision,"'.join(entity) + '" double precision'
+    sql2 = """SELECT  * FROM crosstab ( 'SELECT  "Patient_ID","Key","Value" FROM examination_numerical WHERE "Key" IN ({0})')
+                AS final_result("Patient_ID" TEXT, {1})""".format(entity_fin2, entity_fin3)
+    df = pd.read_sql(sql2, r)
+
     return df
 
 
@@ -31,6 +51,7 @@ def get_num_cat_values(entity_num,entity_cat,r):
     entity2 = ",".join(entity_cat)
     entity_finn = "'" + "','".join(entity_num) + "'"
     entity_finc = "'" + "','".join(entity_cat) + "'"
+
     sql = """SELECT  en."Patient_ID",en."Value" as "{0}",ec."Value" as "{3}" FROM examination_numerical as en 
             left join examination_categorical as ec on en."Patient_ID" = ec."Patient_ID" 
             where en."Key" in ({1}) and ec."Key" in ({2})""".format(entity,entity_finn,entity_finc,entity2)
@@ -43,9 +64,11 @@ def get_num_cat_values(entity_num,entity_cat,r):
 
 
 def get_numeric_entities(rdb):
-
+    t0 = time.time()
     sql = """Select "Key" from name_type where type = 'Double'"""
     df= pd.read_sql(sql, rdb)
-
+    t1 = time.time()
+    total = t1-t0
+    print(total)
     return df['Key']
 
