6cd1faca276930643e30f7552e6122635e5e04ba
Magdalena5 <magdalenasmieszek5@gmail.com>
2020-06-15 10:30:24 +0200

    remove error

--- modules/load_data_postgre.py
+++ modules/load_data_postgre.py
@@ -4,14 +4,14 @@ import time
 
 
 def get_categorical_entities(rdb):
-    sql = """Select "Key" from name_type2 where "type" = 'String' """
+    sql = """Select "Key" from name_type where "type" = 'String' """
     df= pd.read_sql(sql, rdb)
 
     return df['Key']
 
 def number(r):
 
-    sql = """SELECT COUNT (*) FROM examination_categorical2"""
+    sql = """SELECT COUNT (*) FROM examination_categorical"""
     n = pd.read_sql(sql, r)
 
     return n['count']
@@ -20,7 +20,7 @@ def get_values(entity, r):
     t0 = time.time()
     entity_fin = "'" + "','".join(entity) + "'"
 
-    sql = """SELECT  "Patient_ID","Key","Value" FROM examination_numerical2 WHERE "Key" IN ({})""".format(entity_fin)
+    sql = """SELECT  "Patient_ID","Key","Value" FROM examination_numerical WHERE "Key" IN ({})""".format(entity_fin)
     df = pd.read_sql(sql, r)
     df = df.pivot_table(index="Patient_ID", columns="Key", values="Value", aggfunc=np.mean).reset_index()
 
@@ -43,7 +43,7 @@ def get_values(entity, r):
 def get_cat_values(entity,r):
     entity_fin = "'" + "','".join(entity) + "'"
 
-    sql = """SELECT  "Patient_ID","Key","Value" FROM examination_categorical2 WHERE "Key" IN ({}) """.format(entity_fin)
+    sql = """SELECT  "Patient_ID","Key","Value" FROM examination_categorical WHERE "Key" IN ({}) """.format(entity_fin)
     df = pd.read_sql(sql, r)
     df = df.pivot_table(index="Patient_ID", columns="Key", values="Value", aggfunc=min).reset_index()
 
@@ -62,8 +62,8 @@ def get_num_cat_values(entity_num,entity_cat,r):
     entity_finn = "'" + "','".join(entity_num) + "'"
     entity_finc = "'" + "','".join(entity_cat) + "'"
 
-    sql = """SELECT  en."Patient_ID",en."Value" as "{0}",ec."Value" as "{3}" FROM examination_numerical2 as en 
-            left join examination_categorical2 as ec on en."Patient_ID" = ec."Patient_ID" 
+    sql = """SELECT  en."Patient_ID",en."Value" as "{0}",ec."Value" as "{3}" FROM examination_numerical as en 
+            left join examination_categorical as ec on en."Patient_ID" = ec."Patient_ID" 
             where en."Key" in ({1}) and ec."Key" in ({2})""".format(entity,entity_finn,entity_finc,entity2)
 
     df = pd.read_sql(sql, r)
@@ -74,7 +74,7 @@ def get_num_cat_values(entity_num,entity_cat,r):
 
 
 def get_numeric_entities(rdb):
-    sql = """Select "Key" from name_type2 where type = 'Double'"""
+    sql = """Select "Key" from name_type where type = 'Double'"""
     df= pd.read_sql(sql, rdb)
 
     return df['Key']
