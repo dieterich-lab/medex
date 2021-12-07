52be65b5e40976bcaa9f767b1fd7040f181b511a
Magdalena5 <magdalenasmieszek5@gmail.com>
2021-12-07 14:55:23 +0100

    fixed errors

--- url_handlers/boxplot.py
+++ url_handlers/boxplot.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, session
 import modules.load_data_postgre as ps
 import plotly.express as px
 import url_handlers.filtering as filtering
-from webserver import rdb, df_min_max, data, block, all_measurement
+from webserver import rdb, df_min_max, data, block, all_measurement, measurement_name
 import pandas as pd
 
 boxplot_page = Blueprint('boxplot', __name__,template_folder='templates')
