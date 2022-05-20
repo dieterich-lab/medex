4a43cc9e5dcf0786bcf70914b5433a0035f351b5
Magdalena5 <magdalenasmieszek5@gmail.com>
2022-05-20 16:05:54 +0200

    changes in scheduler

--- modules/import_scheduler.py
+++ modules/import_scheduler.py
@@ -4,10 +4,10 @@ import os
 import sys
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
+import tzlocal
 
 
-
-class ImportSettings():
+class ImportSettings:
     """
     Class which create file dev_import necessary to import data.
     Inside the file we have two unique cods for files dataset and entities.
@@ -57,6 +57,7 @@ class ImportSettings():
             return False
         return True
 
+
 def start_import(rdb):
     """ Import data from entities and dataset files"""
 
@@ -65,7 +66,6 @@ def start_import(rdb):
     dataset = './import/dataset.csv'
     entities = './import/entities.csv'
     header = './import/header.csv'
-
 
     if not os.path.isfile(dataset) or not os.path.isfile(entities):
         return print("Could not import to database either or entities.csv and dataset.csv is missing", file=sys.stderr)
@@ -92,14 +92,14 @@ def start_import(rdb):
         print("End load data")
 
 
-class Scheduler():
+class Scheduler:
     """
     BackgroundScheduler runs in a thread inside existing application.
     Importing data check the data. Import data every day at 05.05 if the program see any changes.
     """
 
     def __init__(self,rdb, day_of_week, hour, minute):
-        self.bgs = BackgroundScheduler()
+        self.bgs = BackgroundScheduler(timezone=str(tzlocal.get_localzone()))
         start_import(rdb)
         self.bgs.add_job(start_import,'cron',[rdb], day_of_week=day_of_week, hour=hour, minute=minute)
 
