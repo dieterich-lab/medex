d9e2b696d75006f53d401ff9be2c5232261277b7
sanketahegde <sanketahegde@gmail.com>
2022-12-13 14:40:38 +0100

    cleanup of URL handler files for error messages

--- url_handlers/boxplot.py
+++ url_handlers/boxplot.py
@@ -6,6 +6,7 @@ from modules.get_data_to_histogrm import get_histogram_box_plot
 import plotly.express as px
 import plotly.graph_objects as go
 from url_handlers.filtering import check_for_date_filter_post, check_for_limit_offset
+from url_handlers.utility import _is_valid_entity
 from webserver import block_measurement, all_measurement, start_date, end_date
 import pandas as pd
 import textwrap
@@ -27,67 +28,70 @@ def post_boxplots():
         measurement = [all_measurement[0]]
     else:
         measurement = request.form.getlist('measurement')
-    entities = (request.form.get('numeric_entities'), request.form.get('categorical_entities'),
-                request.form.getlist('subcategory_entities'))
+    numerical_entity = request.form.get('numeric_entities')
+    group_by_entity = request.form.get('categorical_entities')
+    categories = request.form.getlist('subcategory_entities')
     how_to_plot = request.form.get('how_to_plot')
 
     # get_filter
     check_for_date_filter_post(start_date, end_date)
     date_filter = session.get('date_filter')
     limit_filter = check_for_limit_offset()
-    update_filter = session.get('filtering')
 
     # handling errors and load data from database
     df = pd.DataFrame()
-    if measurement == "Search entity":
-        error = "Please select number of measurement"
-    elif entities[0] == "Search entity" or entities[1] == "Search entity":
-        error = "Please select entity"
-    elif not entities[2]:
+    if not measurement:
+        error = "Please select number of visit"
+    elif not _is_valid_entity(numerical_entity):
+        error = "Please select numerical entity"
+    elif not _is_valid_entity(group_by_entity):
+        error = "Please select 'group by' entity"
+    elif not categories:
         error = "Please select subcategory"
     else:
         session_db = get_db_session()
         filter_service = get_filter_service()
-        df, error = get_histogram_box_plot(entities, measurement, date_filter, limit_filter, filter_service, session_db)
+        df, error = get_histogram_box_plot([numerical_entity, group_by_entity, categories], measurement, date_filter,
+                                           limit_filter, filter_service, session_db)
 
     if error:
         return render_template('boxplot.html',
                                error=error,
                                how_to_plot=how_to_plot,
                                measurement=measurement,
-                               numeric_entities=entities[0],
-                               categorical_entities=entities[1],
-                               subcategory_entities=entities[2])
+                               numeric_entities=numerical_entity,
+                               categorical_entities=group_by_entity,
+                               subcategory_entities=categories)
 
     # Plot figure and convert to an HTML string representation
     if block_measurement == 'none':
-        table = df.groupby([entities[1]]).size().reset_index(name='counts')
-        fig_table = go.Figure(data=[go.Table(header=dict(values=list(table[entities[1]].values)),
+        table = df.groupby([group_by_entity]).size().reset_index(name='counts')
+        fig_table = go.Figure(data=[go.Table(header=dict(values=list(table[group_by_entity].values)),
                                              cells=dict(values=table['counts'].transpose().values.tolist()))])
         if how_to_plot == 'linear':
-            fig = px.box(df, x=entities[1], y=entities[0], color=entities[1],
+            fig = px.box(df, x=group_by_entity, y=numerical_entity, color=group_by_entity,
                          template="plotly_white")
         else:
-            fig = px.box(df, x=entities[1], y=entities[0], color=entities[1],
+            fig = px.box(df, x=group_by_entity, y=numerical_entity, color=group_by_entity,
                          template="plotly_white", log_y=True)
     else:
-        table = df.groupby(['measurement', entities[1]]).size().reset_index(name='counts')
-        table = table.pivot(index='measurement', columns=entities[1], values='counts').reset_index()
+        table = df.groupby(['measurement', group_by_entity]).size().reset_index(name='counts')
+        table = table.pivot(index='measurement', columns=group_by_entity, values='counts').reset_index()
         fig_table = go.Figure(data=[go.Table(header=dict(values=list(table.columns)),
                                              cells=dict(values=table.transpose().values.tolist()))
                                     ])
         if how_to_plot == 'linear':
-            fig = px.box(df, x='measurement', y=entities[0], color=entities[1],
+            fig = px.box(df, x='measurement', y=numerical_entity, color=group_by_entity,
                          template="plotly_white")
         else:
-            fig = px.box(df, x='measurement', y=entities[0], color=entities[1],
+            fig = px.box(df, x='measurement', y=numerical_entity, color=group_by_entity,
                          template="plotly_white", log_y=True)
 
-    legend = textwrap.wrap(entities[1], width=20)
+    legend = textwrap.wrap(group_by_entity, width=20)
     fig.update_layout(font=dict(size=16),
                       legend_title='<br>'.join(legend),
                       title={
-                          'text': '<b>' + entities[0] + '</b> by <b>' + entities[1] + '</b>',
+                          'text': '<b>' + numerical_entity + '</b> by <b>' + group_by_entity + '</b>',
                           'x': 0.5,
                           'xanchor': 'center', }
                       )
@@ -98,9 +102,9 @@ def post_boxplots():
 
     return render_template('boxplot.html',
                            measurement=measurement,
-                           numeric_entities=entities[0],
-                           categorical_entities=entities[1],
-                           subcategory_entities=entities[2],
+                           numeric_entities=numerical_entity,
+                           categorical_entities=group_by_entity,
+                           subcategory_entities=categories,
                            how_to_plot=how_to_plot,
                            table=fig_table,
                            plot=fig)
--- url_handlers/histogram.py
+++ url_handlers/histogram.py
@@ -5,6 +5,7 @@ from medex.services.database import get_db_session
 from modules.get_data_to_histogrm import get_histogram_box_plot
 import plotly.express as px
 from url_handlers.filtering import check_for_date_filter_post, check_for_limit_offset
+from url_handlers.utility import _is_valid_entity
 from webserver import all_measurement, measurement_name, block_measurement, start_date, end_date
 import pandas as pd
 import textwrap
@@ -27,28 +28,30 @@ def post_statistics():
         measurement = [all_measurement[0]]
     else:
         measurement = request.form.getlist('measurement')
-    entities = (request.form.get('numeric_entities'), request.form.get('categorical_entities'),
-                request.form.getlist('subcategory_entities'))
+    numerical_entity = request.form.get('numeric_entities')
+    group_by_entity = request.form.get('categorical_entities')
+    categories = request.form.getlist('subcategory_entities')
     number_of_bins = request.form.get('number_of_bins')
 
     # get_filter
     check_for_date_filter_post(start_date, end_date)
     date_filter = session.get('date_filter')
     limit_filter = check_for_limit_offset()
-    update_filter = session.get('filtering')
 
     # handling errors and load data from database
     df = pd.DataFrame()
-    if measurement == "Search entity":
+    if not measurement:
         error = "Please select number of {}".format(measurement_name)
-    elif entities[0] == "Search entity" or entities[1] == "Search entity":
-        error = "Please select entity"
-    elif not entities[2]:
+    elif not _is_valid_entity(numerical_entity):
+        error = "Please select numeric entity"
+    elif not _is_valid_entity(group_by_entity):
+        error = "Please select 'group by' entity"
+    elif not categories:
         error = "Please select subcategory"
     else:
         session_db = get_db_session()
         filter_service = get_filter_service()
-        df, error = get_histogram_box_plot(entities, measurement, date_filter, limit_filter, filter_service, session_db)
+        df, error = get_histogram_box_plot([numerical_entity, group_by_entity, categories], measurement, date_filter, limit_filter, filter_service, session_db)
 
     # handling errors if number of bins is less then 2
     bin_numbers = 20
@@ -63,32 +66,34 @@ def post_statistics():
         return render_template('histogram.html',
                                number_of_bins=number_of_bins,
                                measurement=measurement,
-                               numeric_entities=entities[0],
-                               categorical_entities=entities[1],
-                               subcategory_entities=entities[2],
+                               numeric_entities=numerical_entity,
+                               categorical_entities=group_by_entity,
+                               subcategory_entities=categories,
                                error=error)
 
     if block_measurement == 'none':
-        fig = px.histogram(df, x=entities[0], color=entities[1], barmode='overlay', nbins=bin_numbers,
+        fig = px.histogram(df, x=numerical_entity, color=group_by_entity, barmode='overlay', nbins=bin_numbers,
                            opacity=0.7, template="plotly_white")
     else:
-        fig = px.histogram(df, x=entities[0], facet_row='measurement', color=entities[1],
+        fig = px.histogram(df, x=numerical_entity, facet_row='measurement', color=group_by_entity,
                            barmode='overlay', nbins=bin_numbers, opacity=0.7, template="plotly_white")
 
-    legend = textwrap.wrap(entities[1], width=20)
+    legend = textwrap.wrap(group_by_entity, width=20)
     fig.update_layout(
         font=dict(size=16),
         legend_title='<br>'.join(legend),
         height=1000,
-        title={'text': '<b>' + entities[0] + '</b> by <b>' + entities[1] + '</b>',
+        title={'text': '<b>' + numerical_entity + '</b> by <b>' + group_by_entity + '</b>',
                'x': 0.5,
                'xanchor': 'center'})
     fig = fig.to_html()
     return render_template('histogram.html',
                            number_of_bins=number_of_bins,
                            measurement=measurement,
-                           numeric_entities=entities[0],
-                           categorical_entities=entities[1],
-                           subcategory_entities=entities[2],
+                           numeric_entities=numerical_entity,
+                           categorical_entities=group_by_entity,
+                           subcategory_entities=categories,
                            plot=fig,
                            )
+
+
