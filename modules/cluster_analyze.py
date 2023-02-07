6ad7825be7856b859adbf8babe826249357868b3
Harald Wilhelmi <harald.wilhelmi@uni-heidelberg.de>
2023-02-07 15:29:21 +0100

    Fixing some database setup.
    (cherry picked from commit 5fc815093f7e9f6382724952f811154fd5d1de46)

--- modules/cluster_analyze.py
+++ modules/cluster_analyze.py
@@ -1,19 +1,21 @@
+from sqlalchemy import text
+
 from medex.services.database import get_db_session, get_db_engine
 
 
 def cluster_table():
     db_session = get_db_session()
-    db_session.execute(""" CLUSTER examination_date USING idx_key_date""")
-    db_session.execute(""" CLUSTER examination_numerical USING idx_key_num""")
-    db_session.execute(""" CLUSTER examination_categorical USING idx_key_cat""")
+    db_session.execute(text('CLUSTER examination_date USING idx_key_date'))
+    db_session.execute(text('CLUSTER examination_numerical USING idx_key_num'))
+    db_session.execute(text('CLUSTER examination_categorical USING idx_key_cat'))
     db_session.commit()
 
 
 def analyze_table():
     db_session = get_db_session()
-    db_session.execute(""" ANALYZE examination_numerical""")
-    db_session.execute(""" ANALYZE examination_categorical""")
-    db_session.execute(""" ANALYZE examination_date""")
+    db_session.execute(text('ANALYZE examination_numerical'))
+    db_session.execute(text('ANALYZE examination_categorical'))
+    db_session.execute(text('ANALYZE examination_date'))
     db_session.commit()
 
 
@@ -24,4 +26,4 @@ def alter_system():
         "ALTER SYSTEM SET work_mem='2GB'",
         "ALTER SYSTEM SET max_parallel_workers_per_gather=7",
     ]:
-        c.execute(command)
+        c.execute(text(command))
