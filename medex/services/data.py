f289a00643c26cbfba86d3f832c78fa685cfc063
Harald Wilhelmi <harald.wilhelmi@uni-heidelberg.de>
2023-02-22 11:07:32 +0100

    Removing code duplication

--- medex/services/data.py
+++ medex/services/data.py
@@ -26,6 +26,9 @@ class DataService:
             sort_order: Optional[SortOrder] = None,
     ) -> (List[dict], int):
         query_select = self._get_union_of_tables(entities, measurements)
+        return self._do_query_with_extras(query_select, limit, offset, sort_order)
+
+    def _do_query_with_extras(self, query_select, limit, offset, sort_order):
         query_with_filter = self._filter_service.apply_filter_to_complex_query(query_select)
         query_with_total = self._get_query_with_total(query_with_filter.subquery())
         query_ordered = self._get_ordered_data(query_with_total, sort_order)
@@ -43,12 +46,7 @@ class DataService:
     ) -> (List[dict], int):
         query_select_union = self._get_union_of_tables(entities, measurements)
         query_select = self._get_entities_as_columns(query_select_union, entities)
-        query_with_filter = self._filter_service.apply_filter_to_complex_query(query_select)
-        query_with_total = self._get_query_with_total(query_with_filter.subquery())
-        query_select_with_order = self._get_ordered_data(query_with_total, sort_order)
-        query_with_limit = self._get_query_with_limit(limit, offset, query_select_with_order)
-        result_dict, total = self._get_all_results(query_with_limit)
-        return result_dict, total
+        return self._do_query_with_extras(query_select, limit, offset, sort_order)
 
     @staticmethod
     def _get_union_of_tables(entities, measurements) -> query:
