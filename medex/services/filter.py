a57b115e83b374cf35a5a5055dadcfc9469e5249
Harald Wilhelmi <harald.wilhelmi@uni-heidelberg.de>
2025-06-16 10:47:01 +0200

    Fixing linter issue

--- medex/services/filter.py
+++ medex/services/filter.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Query
 
 from medex.dto.filter import FilterStatus, CategoricalFilter, NumericalFilter
 from medex.services.session import SessionService
-from medex.database_schema import SessionFilteredPatientTable, SessionPatientsMatchingFilterTable,\
+from medex.database_schema import SessionFilteredPatientTable, SessionPatientsMatchingFilterTable, \
     CategoricalValueTable, NumericalValueTable
 
 
